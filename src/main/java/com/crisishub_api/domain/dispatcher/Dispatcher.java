c98408648e5cc9b4d5944c9dbfcb6bd9bea68be8
Łukasz Kośmiński <polde@protonmail.com>
2024-10-10 19:22:22 +0200

    init commit

---
+++ src/main/java/com/crisishub_api/domain/dispatcher/Dispatcher.java
@@ -0,0 +1,4 @@
+package com.crisishub_api.domain.dispatcher;
+
+public class Dispatcher {
+}
---
+++ src/main/java/com/crisishub_api/domain/emergency_services/EmergencyServices.java
@@ -0,0 +1,4 @@
+package com.crisishub_api.domain.emergency_services;
+
+public class EmergencyServices {
+}
