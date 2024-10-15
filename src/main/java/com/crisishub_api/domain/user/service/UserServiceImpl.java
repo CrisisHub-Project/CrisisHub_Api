e35f79f5f842e25f5cf44c22b55dc22cc5d15beb
Tomasz K <bossandmaster69@gmail.com>
2024-10-16 01:23:14 +0200

    Create User detals and add Repository, Controller, Service

---
+++ src/main/java/com/crisishub_api/domain/user/service/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package com.crisishub_api.domain.user.service;
+
+import com.crisishub_api.domain.user.User;
+import com.crisishub_api.domain.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public List<User> fetchUserList() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    @Override
+    public User updateUser(User user, Long userId) {
+//        User userDB = userRepository.findById(userId).get();
+//
+//        if (Objects.nonNull(user.getUserName()) && !"".equalsIgnoreCase(user.getUserName())) {
+//            userDB.setUserName(user.getUserName());
+//        } else if (Objects.nonNull(user.getUserSurname()) && !"".equalsIgnoreCase(user.getUserSurname())) {
+//            userDB.setUserSurname(user.getUserSurname());
+//        } else if
+
+        return null;
+    }
+
+    @Override
+    public void deleteUserById(Long userId) {
+        userRepository.deleteById(userId);
+    }
+}
