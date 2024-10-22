ab041992eca15cda35c617629ebd2084042e3728
Tomasz K <bossandmaster69@gmail.com>
2024-10-22 19:06:13 +0200

    Add Validate to UserCreateDto

--- src/main/java/com/crisishub_api/domain/user/dto/UserCreateDto.java
+++ src/main/java/com/crisishub_api/domain/user/dto/UserCreateDto.java
@@ -1,17 +1,26 @@
 package com.crisishub_api.domain.user.dto;
 
 import com.crisishub_api.domain.user.Role;
+import jakarta.persistence.Enumerated;
 import lombok.Builder;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Builder
 public record UserCreateDto(
-
+        @NotBlank(message = "Pole nie może być puste i musi zawierać przynajmniej jeden znak specjalny")
+        @Size(min = 3, message = "Musi być dłuższe niż 3 znaki")
         String username,
-
+        @NotBlank(message = "Pole nie może być puste i musi zawierać przynajmniej jeden znak specjalny")
+        @Size(min = 7, message = "Musi być dłuższe niż 7 znaków")
         String password,
 
         String confirmPassword,
 
+        @Email
         String email,
 
         Role role
--- src/main/java/com/crisishub_api/domain/user/dto/UserResponseDto.java
+++ src/main/java/com/crisishub_api/domain/user/dto/UserResponseDto.java
@@ -2,6 +2,7 @@ package com.crisishub_api.domain.user.dto;
 
 import com.crisishub_api.domain.user.Role;
 import lombok.Builder;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Builder
 public record UserResponseDto(
